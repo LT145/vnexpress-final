21833f3dcc20bb51e6c9945754ad7a2911b42043
LT145 <loc42286@gmail.com>
2025-04-22 21:34:07 +0700

    13

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
-
-export async function GET(request: Request,) {
+export const dynamic = 'force-dynamic';
+export async function GET() {
   try {
     const comments = await prisma.comment.findMany({
       select: {
@@ -34,7 +34,7 @@ export async function GET(request: Request,) {
 
     return NextResponse.json(formattedComments);
   } catch (error) {
-    console.error('Error fetching comments:', error, request);
+    console.error('Error fetching comments:', error);
     return NextResponse.json(
       { error: 'Failed to fetch comments' },
       { status: 500 }
